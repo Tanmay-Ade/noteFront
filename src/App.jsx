ca63fbef36de41043f0e469a345d6f1da36e1102
Tanmay Ade <tanmayade18@gmail.com>
2024-01-29 16:30:58 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,161 @@
+import { useEffect, useState } from "react";
+import "./app.css";
+
+const App = () => {
+  const [notes, setNotes] = useState([]);
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [selectedNote, setSelectedNote] = useState(null);
+
+  useEffect(() => {
+    const fetchNotes = async () => {
+      try {
+        const response = await fetch("https://noteback-7qxz.onrender.com/api/notes");
+        const notes = await response.json();
+        setNotes(notes);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    fetchNotes();
+  }, []);
+
+  const handleNoteClick = (note) => {
+    setSelectedNote(note);
+    setTitle(note.title);
+    setContent(note.content);
+  };
+
+  const handleAddNote = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await fetch("https://noteback-7qxz.onrender.com/api/notes", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          title,
+          content,
+        }),
+      });
+
+      const newNotes = await response.json();
+
+      setNotes([...notes, newNotes]);
+      setTitle("");
+      setContent("");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleUpdateNote = async (e) => {
+    e.preventDefault();
+    if (!selectedNote) {
+      return;
+    }
+
+    try {
+      const response = await fetch(
+        `https://noteback-7qxz.onrender.com/api/notes/${selectedNote.id}`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            title,
+            content,
+          }),
+        }
+      );
+
+      const updatedNote = await response.json();
+
+      const updatedNotesList = notes.map((note) =>
+        note.id === selectedNote.id ? updatedNote : note
+      );
+
+      setNotes(updatedNotesList);
+      setTitle("");
+      setContent("");
+      setSelectedNote(null);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleCancel = () => {
+    setTitle("");
+    setContent("");
+    setSelectedNote(null);
+  };
+
+  const deleteNote = async (e, noteId) => {
+    e.stopPropagation();
+
+    try {
+      await fetch(`https://noteback-7qxz.onrender.com/api/notes/${noteId}`, {
+        method: "DELETE",
+      })
+
+      const updatedNotes = notes.filter((note) => note.id !== noteId);
+      setNotes(updatedNotes);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className="app-container">
+      <form
+        action=""
+        className="note-form"
+        onSubmit={(e) =>
+          selectedNote ? handleUpdateNote(e) : handleAddNote(e)
+        }
+      >
+        <input
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          placeholder="title"
+          required
+        />
+        <textarea
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+          placeholder="Content"
+          rows={10}
+          required
+        />
+        {selectedNote ? (
+          <div className="edit-buttons">
+            <button type="submit">Save</button>
+            <button onClick={handleCancel}>Cancel</button>
+          </div>
+        ) : (
+          <button type="submit">Add Note</button>
+        )}
+      </form>
+      <div className="notes-grid">
+        {notes.map((note) => (
+          <div
+            key={note.id}
+            className="note-item"
+            onClick={() => handleNoteClick(note)}
+          >
+            <div className="notes-header">
+              <button onClick={(e) => deleteNote(e, note.id)}>x</button>
+            </div>
+            <h2>{note.title}</h2>
+            <p>{note.content}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default App;
